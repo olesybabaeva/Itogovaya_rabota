ee042eb55cabba4e5a0847e124e25e726718f70a
Lesya <146016856+olesybabaeva@users.noreply.github.com>
2024-03-17 03:56:46 -0700

    Update Program.cs
    Создание массива

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -1,1 +1,16 @@
-﻿
+Console.Write("Введите кол-во элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[number];
+
+Console.WriteLine($"Исходный массив: [{string.Join(" , ", CreateArray())}]");
+Console.WriteLine($"Новый массив: [{string.Join(" , ", NewArray(array))}]");
+
+string[] CreateArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива:\t");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
